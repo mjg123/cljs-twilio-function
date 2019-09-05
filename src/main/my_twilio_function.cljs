ad4886271135af2aa1fea61b0e065bf3d4ae0c72
Matthew Gilliard <matthew@gilliard.lol>
2019-09-05 15:27:12 +0100

    Adds the Hickey Quotes and protects the function

--- src/main/my_twilio_function.cljs
+++ src/main/my_twilio_function.cljs
@@ -1,4 +1,13 @@
-(ns my-twilio-function)
+(ns my-twilio-function
+  (:require-macros [hiccups.core :as hiccups :refer [html]])
+  (:require [hiccups.runtime :as hiccupsrt]
+            [quotes :as q]))
 
 (defn my-function [context event callback]
-  (callback nil "This is a ClojureScript Function!"))
+  
+  (callback nil
+            (doto (new js/Twilio.Response)
+              (.appendHeader "content-type" "application/xml")
+              (.setBody (html [:Response
+                               [:Message
+                                (rand-nth q/hickey-quotes)]])))))
