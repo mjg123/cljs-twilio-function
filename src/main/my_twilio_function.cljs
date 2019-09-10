6f17093f1f707e406731a7061773289629db981d
Matthew Gilliard <matthew@gilliard.lol>
2019-09-10 14:29:43 +0100

    Adds the Rich Hickey quotes

--- src/main/my_twilio_function.cljs
+++ src/main/my_twilio_function.cljs
@@ -1,6 +1,7 @@
 (ns my-twilio-function
   (:require-macros [hiccups.core :as hiccups :refer [html]])
-  (:require [hiccups.runtime :as hiccupsrt]))
+  (:require [hiccups.runtime :as hiccupsrt]
+            [quotes :as q]))
 
 (defn my-function [context event callback]
  
@@ -8,4 +9,4 @@
             (doto (new js/Twilio.Response)
               (.appendHeader "content-type" "application/xml")
               (.setBody (html [:Response
-                               [:Message "This is a ClojureScript Function!"]])))))
+                               [:Message (rand-nth q/hickey-quotes)]])))))
---
+++ src/main/quotes.cljs
@@ -0,0 +1,21 @@
+(ns quotes)
+
+;; Selected from https://github.com/Azel4231/clojure-quotes/blob/master/generated.md
+
+(def hickey-quotes
+  ["Design is to take things apart in such a way that they can be put back together"
+   "To the extent you have broken up your problem into reusable parts and composed them, those parts may be separable from your design and useful in another context. And that's how we get reuse."
+   "As a default architecture mutable objects are terrible."
+   "When you know your data can never change out from underneath you, everything is different."
+   "Simplicity is hard work."
+   "If you want everything to be familiar, you'll never learn anything new."
+   "Easiness will eventually slow you down"
+   "Programs are increasingly about code, and decreasingly about data. And I think that is a mistake."
+   "Polymorphism à la carte completely changes the way you work"
+   "Immutability rocks ... It's a profound idea. It's not my idea. It's just an essential characteristic of the universe."
+   "Clojure is in a unique position in providing maybe the friendliest face ever presented to functional programming that's out there"
+   "Now, we're in ClojureScript. Do we need sugar for JavaScript? We're like chocolate syrup for JavaScript...with whipped cream and cherries and sprinkles and everything!"
+   "I was an expert C++ user and really loved C++. For some value of 'love'. That involves no satisfaction at all."
+   "I would encourage you all to embrace the fact that Clojure is different."
+   "I don't believe in disallowing dangerous things. Because sometimes dangerous things are useful"
+   "Programming is not about typing, it's about thinking."])
