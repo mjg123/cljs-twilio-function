ec5cec0d2eae544a55f1f3ab7c67911395800632
Matthew Gilliard <matthew@gilliard.lol>
2019-09-10 14:22:08 +0100

    Adds hiccups and generates TwiML

--- src/main/my_twilio_function.cljs
+++ src/main/my_twilio_function.cljs
@@ -1,4 +1,11 @@
-(ns my-twilio-function)
+(ns my-twilio-function
+  (:require-macros [hiccups.core :as hiccups :refer [html]])
+  (:require [hiccups.runtime :as hiccupsrt]))
 
 (defn my-function [context event callback]
-  (callback nil "This is a ClojureScript Function!"))
+ 
+  (callback nil
+            (doto (new js/Twilio.Response)
+              (.appendHeader "content-type" "application/xml")
+              (.setBody (html [:Response
+                               [:Message "This is a ClojureScript Function!"]])))))
